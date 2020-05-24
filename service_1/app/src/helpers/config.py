0667070e74712daea37e7e81aeb6b79c0e6cfb94
singhdigvijay99 <digvijay.digvijay1998@gmail.com>
2020-05-24 17:39:01 +0530

    done

--- service_1/app/src/helpers/config.py
+++ service_1/app/src/helpers/config.py
@@ -2,4 +2,5 @@ import os
 SERVICE2 = os.environ.get('SERVICE2')
 SERVICE2_PORT = os.environ.get('SERVICE2_PORT')
 
-PRECOMPUTE_SERVICE = 'http://' + SERVICE2 + ':' + SERVICE2_PORT +'/precomputed' 
+#PRECOMPUTE_SERVICE = 'http://' + SERVICE2 + ':' + SERVICE2_PORT +'/precomputed' 
+PRECOMPUTE_SERVICE = 'http://localhost:9090/precomputed'
